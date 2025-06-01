68b4e71d790a701dd557f2955af1ba338a8e0370
krishna-devx <krishna@devxconsultancy.com>
2025-06-02 00:00:59 +0530

    vip validations completed

--- bindings/theme/react/vip-validations/src/sections/vip-validations.jsx
+++ bindings/theme/react/vip-validations/src/sections/vip-validations.jsx
@@ -260,8 +260,331 @@ const CART_UPDATE = `mutation UpdateCart($areaCode: String, $b: Boolean, $buyNow
       }
   }
 }`;
+
+const CART_DETAILS = `query Cart($areaCode: String, $assignCardId: Int, $includeBreakup: Boolean, $buyNow: Boolean, $includeAllItems: Boolean, $includeCodCharges: Boolean, $cartId: String) {
+  cart(areaCode: $areaCode, assignCardId: $assignCardId, includeBreakup: $includeBreakup, buyNow: $buyNow, includeAllItems: $includeAllItems, includeCodCharges: $includeCodCharges, id: $cartId) {
+    buy_now
+    cart_id
+    checkout_mode
+    comment
+    coupon_text
+    delivery_charge_info
+    gstin
+    id
+    is_valid
+    message
+    restrict_checkout
+    user_cart_items_count
+    uid
+    items {
+      bulk_offer
+      coupon_message
+      custom_order
+      discount
+      is_set
+      key
+      message
+      moq
+      parent_item_identifiers
+      product_ean_id
+      quantity
+      availability {
+        deliverable
+        is_valid
+        other_store_quantity
+        out_of_stock
+        sizes
+        available_sizes {
+          display
+          is_available
+          value
+        }
+      }
+      article {
+        _custom_json
+        cart_item_meta
+        extra_meta
+        gift_card
+        identifier
+        is_gift_visible
+        meta
+        mto_quantity
+        parent_item_identifiers
+        product_group_tags
+        quantity
+        seller_identifier
+        size
+        tags
+        uid
+        seller {
+          name
+          uid
+        }
+        price {
+          base {
+            currency_code
+            currency_symbol
+            effective
+            marked
+          }
+          converted {
+            currency_code
+            currency_symbol
+            effective
+            marked
+          }
+        }
+      }
+      price_per_unit {
+        base {
+          currency_code
+          currency_symbol
+          effective
+          marked
+          selling_price
+        }
+        converted {
+          currency_code
+          currency_symbol
+          effective
+          marked
+          selling_price
+        }
+      }
+      product {
+        _custom_json
+        attributes
+        item_code
+        name
+        slug
+        tags
+        type
+        uid
+        brand {
+          name
+          uid
+        }
+        action {
+          type
+          url
+        }
+        categories {
+          name
+          uid
+        }
+        images {
+          aspect_ratio
+          secure_url
+          url
+        }
+      }
+      promo_meta {
+        message
+      }
+      promotions_applied {
+        amount
+        article_quantity
+        code
+        meta
+        mrp_promotion
+        offer_text
+        promo_id
+        promotion_group
+        promotion_name
+        promotion_type
+        applied_free_articles {
+          article_id
+          parent_item_identifier
+          quantity
+        }
+        discount_rules {
+          item_criteria
+          matched_buy_rules
+          offer
+          raw_offer
+        }
+      }
+      charges {
+        meta
+        name
+        allow_refund
+        code
+        type
+        amount {
+          currency
+          value
+        }
+      }
+      coupon {
+        code
+        discount_single_quantity
+        discount_total_quantity
+      }
+      identifiers {
+        identifier
+      }
+      price {
+        base {
+          currency_code
+          currency_symbol
+          effective
+          marked
+        }
+        converted {
+          currency_code
+          currency_symbol
+          effective
+          marked
+        }
+      }
+      delivery_promise {
+        formatted {
+          max
+          min
+        }
+        timestamp {
+          max
+          min
+        }
+        iso {
+          max
+          min
+        }
+      }
+    }
+    breakup_values {
+      coupon {
+        code
+        coupon_type
+        coupon_value
+        description
+        is_applied
+        message
+        minimum_cart_value
+        sub_title
+        title
+        type
+        uid
+        value
+      }
+      display {
+        currency_code
+        currency_symbol
+        display
+        key
+        message
+        value
+        preset
+      }
+      loyalty_points {
+        applicable
+        description
+        is_applied
+        total
+      }
+      raw {
+        cod_charge
+        convenience_fee
+        coupon
+        delivery_charge
+        discount
+        fynd_cash
+        gift_card
+        gst_charges
+        mop_total
+        mrp_total
+        subtotal
+        total
+        vog
+        you_saved
+        total_charge
+      }
+    }
+    coupon {
+      cashback_amount
+      cashback_message_primary
+      cashback_message_secondary
+      coupon_code
+      coupon_description
+      coupon_id
+      coupon_subtitle
+      coupon_title
+      coupon_type
+      coupon_value
+      discount
+      is_applied
+      is_valid
+      message
+      minimum_cart_value
+    }
+    delivery_promise {
+      formatted {
+        max
+        min
+      }
+      timestamp {
+        max
+        min
+      }
+      iso {
+        max
+        min
+      }
+    }
+    applied_promo_details {
+      amount
+      article_quantity
+      code
+      meta
+      mrp_promotion
+      offer_text
+      promo_id
+      promotion_group
+      promotion_name
+      promotion_type
+    }
+    currency {
+      code
+      symbol
+    }
+    success
+  }
+
+  coupons {
+    available_coupon_list {
+      coupon_amount
+      coupon_applicable_message
+      coupon_code
+      coupon_type
+      coupon_value
+      description
+      end_date
+      expires_on
+      is_applicable
+      is_applied
+      is_bank_offer
+      max_discount_value
+      message
+      minimum_cart_value
+      offer_text
+      start_date
+      sub_title
+      title
+    }
+    page {
+      current
+      has_next
+      has_previous
+      total
+      total_item_count
+    }
+  }
+}
+`;
+
+
 export function Component({ props }) {
-  console.log(": drafting 4");
+  console.log(": drafting 5");
   const fpi = useFPI();
   const navigate = useNavigate();
   // const { alert } = useSnackbar();
@@ -347,6 +670,17 @@ export function Component({ props }) {
     return now >= startDate && now <= endDate;
   };
 
+  // Function to fetch cart details
+  async function fetchCartDetails(fpi) {
+    const payload = {
+      buyNow: false,
+      includeAllItems: true,
+      includeCodCharges: true,
+      includeBreakup: true,
+    };
+    return await fpi?.executeGQL?.(CART_DETAILS, payload);
+  }
+
   // Function to remove cart item
   const removeCartItem = async (itemDetails, itemSize, itemIndex) => {
     try {
@@ -381,7 +715,8 @@ export function Component({ props }) {
       if (res?.data?.updateCart?.success) {
         alert("VIP exclusive item removed from cart", "info");
         console.log("Item removed successfully:", itemDetails?.product?.name);
-        window.location.reload();
+        const updatedCartResponse = await fetchCartDetails(fpi);
+        console.log("Updated cart data:", updatedCartResponse);
         //TODO: CALL GET CART HERE AGAIN.
       } else {
         alert(
@@ -450,16 +785,18 @@ export function Component({ props }) {
   const validateProductPageAccess = (activeCampaign, isVipUser) => {
     // Check if we're on a product page
     const currentPath = window.location.pathname;
-
+    console.log("Current path:", currentPath);
     if (
       !currentPath.includes("/product/") ||
       !PRODUCT ||
       !activeCampaign?.products
     ) {
+      console.log("Not on a product page, skipping validation.");
       return;
     }
 
     const productData = PRODUCT;
+    
     const productItemCode = productData?.item_code?.toLowerCase();
 
     console.log("Product page validation:", {
@@ -473,8 +810,18 @@ export function Component({ props }) {
     }
 
     // Create a set of campaign product codes for quick lookup
+    // const campaignProductCodes = new Set(
+    //   activeCampaign.products.map((product) => product.item_code?.toLowerCase())
+    // );
+
+    const activeCampaignTest = [
+      {
+        item_code: "marv5rue_KD",
+      },
+    ];
+
     const campaignProductCodes = new Set(
-      activeCampaign.products.map((product) => product.item_code?.toLowerCase())
+      activeCampaignTest.map((product) => product.item_code?.toLowerCase())
     );
 
     // If product is VIP-only and user is not VIP
