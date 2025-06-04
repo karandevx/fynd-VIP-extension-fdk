3b383cfd110150ad914b44f5eefe9240a05b2bd7
karan <karan181716@gmail.com>
2025-06-04 15:31:59 +0530

    cam-prod

--- server.js
+++ server.js
@@ -541,118 +541,130 @@ promotionRouter.post("/create-campaign", async function view(req, res, next) {
     const promotions = {};
     console.log("userGrpIdMap", userGrpIdMap);
 
+    const getProductUIDs = products
+      .filter(
+        (product) => product && typeof product === "object" && product.uid
+      )
+      .map((product) => Number(product.uid))
+      .filter((uid) => !isNaN(uid));
+
     for (const appId of appIds) {
       const groupObj = (userGrpIdMap[appId] || []).find(
         (obj) => obj.name === groupName
       );
       console.log("groupObj", groupObj?.groupId);
-      const prmotionPayload = {
-        buy_rules: {
-          "rule#1": {
-            item_id: products,
-          },
-        },
-        is_processed: true,
-        auto_apply: true,
-        discount_rules: [
-          {
-            discount_type: discount?.type,
-            offer: {
-              ...(discount?.type === "percentage" && {
-                discount_percentage: discount?.value,
-              }),
-              ...(discount?.type === "amount" && {
-                discount_amount: discount?.value,
-              }),
-            },
-            buy_condition: "( rule#1 )",
-            item_criteria: {
-              item_id: products,
-            },
-            buying_conditions_arr: [["rule#1"]],
-          },
-        ],
-        apply_exclusive: "cart",
-        apply_all_discount: false,
-        stackable: true,
-        promo_group: "product",
-        apply_priority: 1,
-        mode: "promotion",
-        visiblility: {
-          coupon_list: false,
-          pdp: true,
-        },
-        restrictions: {
-          uses: {
-            remaining: {
-              user: 0,
-              total: 0,
-            },
-            maximum: {
-              user: 0,
-              total: 0,
+      if (!groupObj?.groupId) {
+        continue; // Skip if no group found for this appId
+      } else {
+        const prmotionPayload = {
+          buy_rules: {
+            "rule#1": {
+              item_id: getProductUIDs,
             },
           },
-          post_order: {
-            return_allowed: true,
-            cancellation_allowed: true,
+          is_processed: true,
+          auto_apply: true,
+          discount_rules: [
+            {
+              discount_type: discount?.type,
+              offer: {
+                ...(discount?.type === "percentage" && {
+                  discount_percentage: discount?.value,
+                }),
+                ...(discount?.type === "amount" && {
+                  discount_amount: discount?.value,
+                }),
+              },
+              buy_condition: "( rule#1 )",
+              item_criteria: {
+                item_id: getProductUIDs,
+              },
+              buying_conditions_arr: [["rule#1"]],
+            },
+          ],
+          apply_exclusive: "cart",
+          apply_all_discount: false,
+          stackable: true,
+          promo_group: "product",
+          apply_priority: 1,
+          mode: "promotion",
+          visiblility: {
+            coupon_list: false,
+            pdp: true,
           },
-          platforms: ["web", "android", "ios"],
-          user_groups: [groupObj?.groupId],
-          user_id: [],
-          payments: {},
-          user_registered: {
-            start: null,
-            end: null,
+          restrictions: {
+            uses: {
+              remaining: {
+                user: 0,
+                total: 0,
+              },
+              maximum: {
+                user: 0,
+                total: 0,
+              },
+            },
+            post_order: {
+              return_allowed: true,
+              cancellation_allowed: true,
+            },
+            platforms: ["web", "android", "ios"],
+            user_groups: [groupObj?.groupId],
+            user_id: [],
+            payments: {},
+            user_registered: {
+              start: null,
+              end: null,
+            },
+            ordering_stores: [],
+            user_type: "user_group",
+            email_domain: [],
           },
-          ordering_stores: [],
-          user_type: "user_group",
-          email_domain: [],
-        },
-        post_order_action: {
-          action_type: null,
-          action_date: null,
-        },
-        display_meta: {
-          name: name,
-          description: description,
-          offer_text: offerText,
-          offer_label: offerLabel,
-        },
-        reason: null,
-        promotion_type: discount?.type,
-        application_id: appId,
-        _custom_json: {},
-        _schedule: {
-          start: startDate,
-          end: endDate,
-          cron: null,
-          duration: 0,
-          published: true,
-          status: "approved",
-        },
-        ownership: {
-          payable_category: "seller",
-          payable_by: null,
-        },
-        calculate_on: "esp",
-      };
-      console.log("prmotionPayload", prmotionPayload);
-      try {
-        const response = await axios.post(
-          `https://api.fynd.com/service/platform/cart/v1.0/company/${companyId}/application/${appId}/promotion`,
-          prmotionPayload,
-          config
-        );
-        console.log("response", response);
+          post_order_action: {
+            action_type: null,
+            action_date: null,
+          },
+          display_meta: {
+            name: name,
+            description: description,
+            offer_text: offerText,
+            offer_label: offerLabel,
+          },
+          reason: null,
+          promotion_type: discount?.type,
+          application_id: appId,
+          _custom_json: {},
+          _schedule: {
+            start: startDate,
+            end: endDate,
+            cron: null,
+            duration: 0,
+            published: true,
+            status: "approved",
+          },
+          ownership: {
+            payable_category: "seller",
+            payable_by: null,
+          },
+          calculate_on: "esp",
+        };
+        console.log("prmotionPayload", prmotionPayload);
+        try {
+          const response = await axios.post(
+            `https://api.fynd.com/service/platform/cart/v1.0/company/${companyId}/application/${appId}/promotion`,
+            prmotionPayload,
+            config
+          );
+          console.log("response", response);
 
-        promotions[appId] = response?.data?._id;
-      } catch (err) {
-        console.error(`Failed for app ${appId}:`, err?.message || err);
-        failedApps.push({ appId });
+          promotions[appId] = response?.data?._id;
+        } catch (err) {
+          console.error(`Failed for app ${appId}:`, err?.message || err);
+          failedApps.push({ appId });
+        }
       }
     }
     const campaignId = getUnique6Digit();
+
     const payload = {
       campaignId: campaignId,
       companyId: companyId,
